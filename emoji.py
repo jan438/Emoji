33c84fd3f025ce61c46ee106f08b5a099bb62a69
jan438 <jan438@users.noreply.github.com>
2024-09-01 14:58:05 +0200

    hex twice

--- emoji.py
+++ emoji.py
@@ -30,7 +30,7 @@ styles = getSampleStyleSheet()
 normalStyle = ParagraphStyle('nrm', parent=styles['Normal'], fontName = nfn, fontSize = 24)
 emojiStyle = ParagraphStyle('emo', parent=styles['Normal'], fontName = efn, fontSize = 24)
 normalParagraph = Paragraph("hallo" + "<font name = " + nfn + ">" + "abc" + "</font>", normalStyle )
-emojiParagraph = Paragraph("<font name = " + nfn + ">" + "hallo" + "</font>" + "😎😛🙈🏈" + u"\u26BD" + "\U0001F339" + u"\xbc" +  u"\u2022" + "<font name = " + nfn + ">" + "hallo" + "</font>", emojiStyle )
+emojiParagraph = Paragraph("<font name = " + nfn + ">" + "hallo" + "</font>" + "😎😛🙈🏈" + u"\u26BD" + "\U0001F339" + u"\xbc" +  u"\xbc\xbc" + u"\u2022" + "<font name = " + nfn + ">" + "hallo" + "</font>", emojiStyle )
 emojiname = "PDF/Emoji.pdf"
 doc = SimpleDocTemplate(emojiname, pagesize=landscape(A4), rightMargin=5, leftMargin=5, topMargin=5, bottomMargin=5)
 storypdf=[]
